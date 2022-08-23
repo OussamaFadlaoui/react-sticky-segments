c0edfbf408856a6a1588b5026b4b17089a3f08e8
Oussama Fadlaoui-School <School@192.168.123.68>
2022-08-23 21:07:09 +0200

    Project setup

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.useExampleHook = void 0;
+const useExampleHook = () => console.log('Succeeded');
+exports.useExampleHook = useExampleHook;
