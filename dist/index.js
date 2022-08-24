30569de0e1f5ebae9ee4765ad2f6e1893fe07fa0
Oussama Fadlaoui <me@oussamafadlaoui.com>
2022-08-24 18:53:27 +0200

    Clean up dist folder

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useExampleHook = void 0;
-const useExampleHook = () => console.log('Succeeded');
-exports.useExampleHook = useExampleHook;
